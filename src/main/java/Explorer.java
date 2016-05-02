e90519ba7abfaeda2cbfeb54710cb581504df978
kamilzaharan <189816@edu.p.lodz.pl>
2016-05-02 15:41:36 +0200

    initial commit Kamil

--- src/main/java/Explorer.java
+++ src/main/java/Explorer.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
-
-/**
- * Created by kamil on 5/1/16.
- */
 public class Explorer {
 	public static int[][] ResultArray = new int[][]{
 		{1, 2, 3, 4},
@@ -40,11 +38,11 @@ public class Explorer {
 						if (childNode.getDirection() == nodeOrder[i]) {
 							System.out.println("stworzylem wezel ale nie wiem czy nalezy"+childNode.getDirection());
 							childNode.printArray();
-							    if (!searchedNode.contains(childNode)){
+							if (!searchedNode.contains(childNode)){
 								System.out.println("Sprawdzilem-- Nie nalezy"+ childNode.getDirection());
 								searchedNode.add(childNode);
 								stackNode.push(childNode);}
-							
+
 						}
 					}
 			}
@@ -55,5 +53,44 @@ public class Explorer {
 			System.out.println("nie znalazlem rozwiazania");
 		}
 
+	}
+
+	public void BFS(Node firstNode, char[] nodeOrder) throws InterruptedException {
+		int depth = 0;
+		List<Node> childrenList = new ArrayList<Node>();
+		List<Node> searchedNode = new ArrayList<Node>();
+		Queue<Node> queueNode = new LinkedList<>();
+		queueNode.add(firstNode);
+		Node resultNode = new Node(ResultArray);
+		boolean rozwiazanie = false;
+		do {
+			Node actualNode = queueNode.remove();
+			if (!searchedNode.contains(actualNode))
+				searchedNode.add(actualNode);
+			if (actualNode.equals(resultNode)) {
+				//saveFile("plik.txt", actualNode.getArray());  //Sprawdzenie czy aktualny wezel‚ nie jest rozwiazaniem
+				rozwiazanie = true;
+				System.out.println("znalazlem rozwiazanie po przeszukaniu " + searchedNode.size() + "wezlow");
+				break;
+			} else {
+				childrenList = actualNode.generateChildren();
+				for (int i = 0; i < nodeOrder.length; i++)
+					for (Node childNode : childrenList) {
+						if (childNode.getDirection() == nodeOrder[i]) {
+							System.out.println("stworzylem wezel ale nie wiem czy nalezy"+childNode.getDirection());
+							childNode.printArray();
+							if (!searchedNode.contains(childNode)){
+								System.out.println("Sprawdzilem-- Nie nalezy"+ childNode.getDirection());
+								searchedNode.add(childNode);
+								queueNode.add(childNode);}
+						}
+					}
+			}
+			//Thread.sleep(100);
+		}
+		while (!queueNode.isEmpty());
+		if (rozwiazanie == false) {
+			System.out.println("nie znalazlem rozwiazania");
+		}
 	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,27 @@ import java.io.FileNotFoundException;
  */
 public class Main {
 
+<<<<<<< HEAD
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         String sourcePath = "input.txt";
         String exploringMethod = "dfs";
         char[] exploringOrder = {'L','P','G','D'};
         Explorer explorer = new Explorer();
+=======
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		String sourcePath = "input.txt";
+		String exploringMethod = "bfs";
+		char[] exploringOrder = {'G','L','P','D'};
+		Explorer explorer = new Explorer();
+>>>>>>> c4bd5e415855638594f0c29525a731608b254ed4
 
-        switch (exploringMethod) {
-            case "dfs":
-                explorer.DFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
-                break;
-            case "bfs":
-
-                break;
-        }
-    }
+		switch (exploringMethod) {
+		case "dfs":
+			explorer.DFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
+			break;
+		case "bfs":
+			explorer.BFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
+			break;
+		}
+	}
 }
