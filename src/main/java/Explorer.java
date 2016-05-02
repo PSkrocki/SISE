fc3977e2dd39ab97645846c5bcfbf3c42a1b1800
PSkrocki <ppskrocki@gmail.com>
2016-05-02 15:58:33 +0200

    Dodanie heurystyki manhattan, interfejsu heurystyk i implementacja bfsheuristic

--- src/main/java/Explorer.java
+++ src/main/java/Explorer.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Explorer {
-	public static int[][] ResultArray = new int[][]{
+	public static int[][] resultArray = new int[][]{
 		{1, 2, 3, 4},
 		{5, 6, 7, 8},
 		{9, 10, 11, 12},
@@ -20,7 +20,7 @@ public class Explorer {
 		List<Node> searchedNode = new ArrayList<Node>();
 		Stack<Node> stackNode = new Stack<Node>();
 		stackNode.push(firstNode);
-		Node resultNode = new Node(ResultArray);
+		Node resultNode = new Node(resultArray);
 		boolean rozwiazanie = false;
 		do {
 			Node actualNode = stackNode.pop();
@@ -61,7 +61,7 @@ public class Explorer {
 		List<Node> searchedNode = new ArrayList<Node>();
 		Queue<Node> queueNode = new LinkedList<>();
 		queueNode.add(firstNode);
-		Node resultNode = new Node(ResultArray);
+		Node resultNode = new Node(resultArray);
 		boolean rozwiazanie = false;
 		do {
 			Node actualNode = queueNode.remove();
@@ -85,6 +85,56 @@ public class Explorer {
 								queueNode.add(childNode);}
 						}
 					}
+			}
+			//Thread.sleep(100);
+		}
+		while (!queueNode.isEmpty());
+		if (rozwiazanie == false) {
+			System.out.println("nie znalazlem rozwiazania");
+		}
+	}
+
+	public void BFSWithHeuristic(Node firstNode, IHeuristic heuristic) throws InterruptedException {
+		int depth = 0;
+		List<Node> childrenList = new ArrayList<Node>();
+		List<Node> searchedNode = new ArrayList<Node>();
+		Queue<Node> queueNode = new LinkedList<>();
+		queueNode.add(firstNode);
+		Node resultNode = new Node(resultArray);
+		boolean rozwiazanie = false;
+		do {
+			Node actualNode = queueNode.remove();
+			if (!searchedNode.contains(actualNode))
+				searchedNode.add(actualNode);
+			if (actualNode.equals(resultNode)) {
+				//saveFile("plik.txt", actualNode.getArray());  //Sprawdzenie czy aktualny wezel‚ nie jest rozwiazaniem
+				rozwiazanie = true;
+				System.out.println("znalazlem rozwiazanie po przeszukaniu " + searchedNode.size() + "wezlow");
+				break;
+			} else {
+
+				childrenList = actualNode.generateChildren();
+				int[] nodeOrder= new int[childrenList.size()];
+				int i=0;				
+				for (Node childNode : childrenList){
+					childNode.setOrder(heuristic.calculateOrders((childNode.getArray())));
+					nodeOrder[i]=heuristic.calculateOrders(childNode.getArray());
+					System.out.println("przydzielono mi kolejnosc:"+nodeOrder[i]);
+					i+=i;
+				}
+				Arrays.sort(nodeOrder);
+				for (int j = 0; j < nodeOrder.length; j++){
+					for (Node childNode : childrenList) {
+						if (childNode.getOrder() == nodeOrder[j]) {
+							System.out.println("stworzylem wezel ale nie wiem czy nalezy"+childNode.getOrder());
+							childNode.printArray();
+							if (!searchedNode.contains(childNode)){
+								System.out.println("Sprawdzilem-- Nie nalezy"+ childNode.getOrder());
+								searchedNode.add(childNode);
+								queueNode.add(childNode);}
+						}
+					}
+				}
 			}
 			//Thread.sleep(100);
 		}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import java.io.FileNotFoundException;
 
-/**
- * Created by kamil on 5/1/16.
- */
+
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		String sourcePath = "input.txt";
-		String exploringMethod = "bfs";
+		String exploringMethod = "bfsh";
 		char[] exploringOrder = {'G','L','P','D'};
 		Explorer explorer = new Explorer();
 
@@ -17,6 +15,9 @@ public class Main {
 			break;
 		case "bfs":
 			explorer.BFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
+			break;
+		case "bfsh":
+			explorer.BFSWithHeuristic(new Node(Serializer.loadFile(sourcePath)),new Manhattan());
 			break;
 		}
 	}
