7590de3bca6a5093b238c67df9a1a376d9db0005
PSkrocki <ppskrocki@gmail.com>
2016-05-02 14:10:12 +0200

    sprzatanie repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,19 @@ import java.io.FileNotFoundException;
  */
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        String sourcePath = "input.txt";
-        String exploringMethod = "dfs";
-        char[] exploringOrder = {'D','G','L','P'};
-        Explorer explorer = new Explorer();
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		String sourcePath = "input.txt";
+		String exploringMethod = "bfs";
+		char[] exploringOrder = {'G','L','P','D'};
+		Explorer explorer = new Explorer();
 
-        switch (exploringMethod) {
-            case "dfs":
-                explorer.DFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
-                break;
-            case "bfs":
-
-                break;
-        }
-    }
+		switch (exploringMethod) {
+		case "dfs":
+			explorer.DFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
+			break;
+		case "bfs":
+			explorer.BFS(new Node(Serializer.loadFile(sourcePath)), exploringOrder);
+			break;
+		}
+	}
 }
